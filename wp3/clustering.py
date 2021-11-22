b1a79aaae3df812c3cc7d6f1f12b84812b8ce470
Moritz Hobein <moritz.hobein@bio.uni-giessen.de>
2021-11-22 15:03:25 +0100

    test script to take a look at the test data

--- wp3/clustering.py
+++ wp3/clustering.py
@@ -1,3 +1,24 @@
-//testing git
+import bamnostic as bs
 
-print("Hier kommt ein Skript zum Erstellen von .bam files pro Cluster hin")
+#just for testing how to work with the VMs 
+
+#input files, will change to argsparse later on
+bam = input(".bam file")
+tsv = input(".tsv file for cluster identification")
+
+#opening the files
+allCells = bs.AlignmentFile(bam,"rb")
+clusterIDs = open(tsv, "r")
+
+for i, read in enumerate(allCells):
+  if(i >= 3):
+    break
+  print(read)
+
+for i, ID in enumerate(clusterIDs):
+  if(i >= 3):
+    break
+  print(ID)
+
+allCells.close()
+clusterIDs.close()
