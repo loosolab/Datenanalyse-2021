9f92c4280a06f8ee90d6f70d475cc5c2a82be6e9
mrl000 <marlin.hanstein@protonmail.com>
2021-11-26 13:52:43 +0100

    added debug prints

--- wp3/cluster.py
+++ wp3/cluster.py
@@ -36,7 +36,7 @@ def getSingleClusterBarcodeList (clusterID,combinedIDs):
 def writeClusterBam (clusterID,cellIDsForCluster,sourceFilePath,outputDir):
     #debug print
     print("working on cluster: "+str(clusterID))
-    print("Debug, cellbacodes for this cluster: "+cellIDsForCluster)
+    print("Debug, cellbacodes for this cluster: "+str(cellIDsForCluster))
     sourceFile = ps.AlignmentFile(sourceFilePath,"rb")
     #TODO bamnostic handles some things different to pysam. next line needs correction (template is not known)
     clusterFile = ps.AlignmentFile(outputDir+"cluster"+str(clusterID)+".bam","wb",template=sourceFile)
