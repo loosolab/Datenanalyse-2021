baedcc08d32dfd7f14c49da002db083734b18f69
Micha Frederick Keßler <micha.frederick.kessler@bioinfsys.uni-giessen.de>
2022-01-02 13:20:51 +0000

    New comments, replaced snap object y with x ap file

--- preprocessing_WP2.R
+++ preprocessing_WP2.R
@@ -1,20 +1,30 @@
 library(SnapATAC);
-y.sp = createSnap(
-  file="/home/rstudio/workspaces/stud4/SnaptoolsTest/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver.snap",
+# use  one .snap file only...
+s_file="/home/rstudio/workspaces/stud4/SnaptoolsTest/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver.snap"
+x.sp = createSnap(
+  file=s_file,
   sample="ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver",
-  num.cores=5
+  num.cores=6
 );
-print(y.sp)
+x.sp
 
-showBinSizes("/home/rstudio/workspaces/stud4/SnaptoolsTest/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver.snap");
-y.sp = addBmatToSnap(y.sp, bin.size=5000, num.cores=5);
-y.sp = makeBinary(y.sp, mat="bmat");
+# ...or merge .snap files first
+file.list = c("/home/rstudio/workspaces/stud4/SnaptoolsTest/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver.snap", 
+              "/home/rstudio/workspaces/stud4/SnaptoolsTest/ENC-1JKYN-020-SM-C1PX3_snATAC_thoracic_aorta.snap");
+sample.list = c("liver", "aorta");
+x.sp = createSnap(file=file.list, sample=sample.list);
+x.sp
 
-y.sp
+# add bin matrix
+# showBinSizes(s_file);
+x.sp = addBmatToSnap(x.sp, bin.size=5000, num.cores=6);
+x.sp = makeBinary(x.sp, mat="bmat");
+x.sp
 
-bin.cov = log10(Matrix::colSums(y.sp@bmat)+1);
-bin.cov
+# calc log10(bin coverage)
+bin.cov = log10(Matrix::colSums(x.sp@bmat)+1);
 
+# plot histogram of bin coverage
 hist(
   bin.cov[bin.cov > 0], 
   xlab="log10(bin cov)", 
@@ -23,20 +33,22 @@ hist(
   xlim=c(0, 5)
 );
 
+# filter bins with low coverage
 bin.cutoff = quantile(bin.cov[bin.cov > 0], 0.95);
 idy = which(bin.cov <= bin.cutoff & bin.cov > 0);
-y.sp = y.sp[, idy, mat="bmat"];
-y.sp
+x.sp = x.sp[, idy, mat="bmat"];
+x.sp
 
-
-y.sp = runDiffusionMaps(
-  obj=y.sp,
+# dimensional reduction
+x.sp = runDiffusionMaps(
+  obj=x.sp,
   input.mat="bmat", 
   num.eigs=50
 );
 
+# show first 25 pairs of eigenvectors
 plotDimReductPW(
-  obj=y.sp, 
+  obj=x.sp, 
   eigs.dims=1:50,
   point.size=0.3,
   point.color="grey",
@@ -48,52 +60,40 @@ plotDimReductPW(
   pdf.width=7
 );
 
-y.sp = runKNN(
-  obj=y.sp,
-  eigs.dims=1:7,
+v_pair = 10
+
+# continue dimensional reduction with first n eigenvectors
+x.sp = runKNN(
+  obj=x.sp,
+  eigs.dims=1:v_pair, # choose first plot with "blob"
   k=15
 );
 
-y.sp=runCluster(
-  obj=y.sp,
+# perform clustering
+x.sp=runCluster(
+  obj=x.sp,
   tmp.folder=tempdir(),
   louvain.lib="R-igraph",
   seed.use=10
 );
 
-y.sp@metaData$cluster = y.sp@cluster;
+x.sp@metaData$cluster = x.sp@cluster;
 
-y.sp = runViz(
-  obj=y.sp, 
+# plot t-sne
+x.sp = runViz(
+  obj=x.sp, 
   tmp.folder=tempdir(),
   dims=2,
-  eigs.dims=1:7, 
+  eigs.dims=1:v_pair, 
   method="Rtsne",
   seed.use=10
 );
 
 par(mfrow = c(2, 2));
 
-plotViz(
-  obj=y.sp,
-  method="tsne", 
-  main="Right lobe of liver Cluster",
-  point.color=y.sp@cluster, 
-  point.size=1, 
-  point.shape=19, 
-  point.alpha=0.8, 
-  text.add=TRUE,
-  text.size=1.5,
-  text.color="black",
-  text.halo.add=TRUE,
-  text.halo.color="white",
-  text.halo.width=0.2,
-  down.sample=10000,
-  legend.add=FALSE
-);
-
-y.sp@barcode
-ca_table = do.call(rbind, Map(data.frame, cell_name=y.sp@barcode, cluster=y.sp@cluster))
+# generate cluster assignment table
+x.sp@barcode
+ca_table = do.call(rbind, Map(data.frame, cell_name=x.sp@barcode, cluster=x.sp@cluster))
 write.table(ca_table,"cluster_assignment_table.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
 ca_table
