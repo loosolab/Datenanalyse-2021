746670f8c0b1151dd09c5d7f22319f68370f84dc
Micha Frederick Keßler <micha.frederick.kessler@bioinfsys.uni-giessen.de>
2022-01-07 11:09:26 +0000

    First steps of gene annotation added

--- preprocessing_WP2.R
+++ preprocessing_WP2.R
@@ -1,4 +1,6 @@
+library(GenomicRanges)
 library(SnapATAC);
+genefile = "/home/rstudio/workspaces/stud4/genefile/gencode.hg38.gene.bed"
 # use  one .snap file only...
 s_file="/home/rstudio/workspaces/stud4/SnaptoolsTest/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver.snap"
 x.sp = createSnap(
@@ -60,6 +62,7 @@ plotDimReductPW(
   pdf.width=7
 );
 
+# select first pair that looks like a blob
 v_pair = 10
 
 # continue dimensional reduction with first n eigenvectors
@@ -109,6 +112,52 @@ plotViz(
   down.sample=10000,
   legend.add=FALSE
 );
+
+# gene annotation
+genes = read.table(genefile);
+genes.gr = GRanges(genes[,1], 
+                     IRanges(genes[,2], genes[,3]), name=genes[,4]
+);
+marker.genes = c(
+  "Snap25", "Gad2", "Apoe",
+  "C1qb", "Pvalb", "Vip", 
+  "Sst", "Lamp5", "Slc17a7"
+);
+genes.sel.gr <- genes.gr[which(genes.gr$name %in% marker.genes)];
+# re-add the cell-by-bin matrix to the snap object;
+x.sp = addBmatToSnap(x.sp);
+x.sp = createGmatFromMat(
+  obj=x.sp, 
+  input.mat="bmat",
+  genes=genes.sel.gr,
+  do.par=TRUE,
+  num.cores=10
+);
+# normalize the cell-by-gene matrix
+x.sp = scaleCountMatrix(
+  obj=x.sp, 
+  cov=x.sp@metaData$passed_filters + 1,
+  mat="gmat",
+  method = "RPM"
+);
+# smooth the cell-by-gene matrix
+x.sp = runMagic(
+  obj=x.sp,
+  input.mat="gmat",
+  step.size=3
+);
+par(mfrow = c(3, 3));
+for(i in 1:9){
+  plotFeatureSingle(
+    obj=x.sp,
+    feature.value=x.sp@gmat[, marker.genes[i]],
+    method="tsne", 
+    main=marker.genes[i],
+    point.size=0.1, 
+    point.shape=19, 
+    down.sample=10000,
+    quantiles=c(0, 1)
+  )};
 
 # generate cluster assignment table
 x.sp@barcode
