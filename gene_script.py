694ca13e7386239de5515bb63afeddbc054bcccd
Mfkessler <79805670+Mfkessler@users.noreply.github.com>
2022-01-12 11:53:22 +0100

    Update gene_script.py
    annotating for all clusters in folder implemented, $1: folder of .narrowPeaks, $2: path of gencode file

--- gene_script.py
+++ gene_script.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import pandas as pd
 
 
@@ -6,8 +9,7 @@ def overlaps(a, b):
     return min(a[1], b[1]) - max(a[0], b[0])
 
 
-def get_genecode():
-    path = "/home/mfk/Google Drive/JLU/1. Semester/Datenanalyse/Gene_script/gencode.filtered.bed"
+def get_gencode(path):
     table = pd.read_csv(path, sep=" ", names=["Chr", "Start", "End", "Gene"])
 
     return table.values.tolist()
@@ -18,7 +20,7 @@ def get_cluster(path):
                                                "p value", "q value", "Peak"])
     table.drop(["Name", "Score", "Strand", "p value", "q value", "Peak"], axis=1, inplace=True)
     table.sort_values(by="Signal Value", ascending=False, inplace=True)
-    table = table.head(10)
+    table = table.head(100)
 
     return table.values.tolist()
 
@@ -30,18 +32,39 @@ def annotate_genes(top_peaks, gene_table):
         for gene in gene_table:
             if line[0] == gene[0]:
                 if overlaps([line[1], line[2]], [gene[1], gene[2]]) > 0:
-                    if gene[3] not in new_line:
+                    if len(new_line) == 4:
                         new_line.append(gene[3])
+                    elif gene[3] not in new_line[4]:
+                        new_line[4] = new_line[4] + ", " + gene[3]
+
         merged_lines.append(new_line)
 
     return merged_lines
 
 
-def main():
-    pd.set_option('display.max_columns', None)
-    cluster1_peaks = "/home/mfk/Google Drive/JLU/1. Semester/Datenanalyse/Gene_script/right-lobe-of-liver.1_peaks.narrowPeak"
+def write_annotated_cluster(lines, file_name):
+    with open(file_name, "w") as cluster_file:
+        for line in lines:
+            for col in line:
+                cluster_file.write(str(col) + "\t")
+            cluster_file.write("\n")
 
-    print(annotate_genes(get_cluster(cluster1_peaks), get_genecode()))
+
+def write_clusters():
+    cluster_folder = sys.argv[1]
+    cluster_files = os.listdir(cluster_folder)
+    gene_file = sys.argv[2]
+    print("Reading " + gene_file)
+    gencode = get_gencode(gene_file)
+    for file in cluster_files:
+        cluster_peaks = get_cluster(cluster_folder + "/" + file)
+        write_annotated_cluster(annotate_genes(cluster_peaks, gencode), file.split("_")[0] + ".peaks.annotated.bed")
+        print(file + " annotated")
+
+
+def main():
+    write_clusters()
+    exit(0)
 
 
 if __name__ == '__main__':
