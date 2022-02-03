e1a58ab439d2553f4624e6f23178b72d4097b51b
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-02-03 23:41:45 +0100

    bug fix in renaming script

--- wp5/scripts/rename_motifs_auto.sh
+++ wp5/scripts/rename_motifs_auto.sh
@@ -2,6 +2,7 @@
 
 # suffix for found motifs
 PATTERN=$1
-# motif file
-MTF=$2
-sed -E -i "s/motif_([[:digit:]]+)       motif_([[:digit:]]+)/motif_\1_${VAR1}    motif_\2_${VAR1}/" ${MTF}
+
+for FILE in "${@:2}"; do
+    sed -E -i "s/motif_([[:digit:]]+)([[:blank:]]+)motif_([[:digit:]]+)/${PATTERN}_\1\2${PATTERN}_\3/" ${FILE}
+done
