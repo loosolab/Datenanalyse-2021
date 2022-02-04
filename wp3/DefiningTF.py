6eb7985a12353e7441075ce7a48c4d64288beda2
Moritz Hobein <moritz.hobein@bio.uni-giessen.de>
2022-02-04 13:53:03 +0100

    first Version of script to gruop TFs by families

--- wp3/DefiningTF.py
+++ wp3/DefiningTF.py
@@ -14,13 +14,16 @@ def cliParser():
     parser.add_argument('-n', '--normalize', dest='Norm', default='Sum', help='Method of normalization of the data.')
     parser.add_argument('-o', '--outputName', dest='Custom_filename', default="ClusterComparison", help='Sets a custom name for saving the output files')
     parser.add_argument('-z', '--zscore', dest='Z', default=True, action='store_false', help='If used, the output will contain the absolute values (or those normlaized) and no z-Scores')
+    parser.add_argument('-f', '--families', dest='families', default=False, action='store_true', help='If used, output will group TFs by families')
     args = parser.parse_args()
     
-    return args.File
+    return args.families
 
 def main():
     
-    inputFile = "comp.tsv"
+    Families = cliParser()
+    inputFile = "ClusterComparison.tsv"
+    #inputFile = "comp.tsv"
 
     with open(inputFile, "r") as file:
         
@@ -40,23 +43,50 @@ def main():
 
     clusters = {}
     
-    for col in df:
-        clusters[col] = []
     
-    print(clusters)
+    #print(clusters)
     
+    collapseCols = {}
     
+    dfT = df.transpose()
+    
+    if Families: 
+        with open("TF_families.txt", "r") as file:
+            for col in dfT:
+                #print(dfT)
+                for line in file:
+                    family, tfactors = line.split("\t")
+                    tfactorList = tfactors.split(",")
+                    if col in tfactorList:
+                            print(1)
+                            collapseCols[col] = family
+         
+        dfFamilies = dfT
+        print(dfFamilies)
+        dfFamilies = dfFamilies.groupby(collapseCols, axis = 1).mean()
+        print(dfFamilies)
+        dfFamilies = dfFamilies.transpose()
+        print(dfFamilies)
+        
+        
+        df = dfFamilies
+            
+    print(collapseCols)    
+    print(df)   
     #Quantilebestimmung und Extraktion der relevanten TF 
     quants = df.quantile(.95)
     
-    print(quants["cluster9_Score"])
+    
+    for col in df:
+        clusters[col] = []
+    #print(quants["cluster9_Score"])
     
     for col in df:
         for index in df.index:
            if df.at[index, col] >= quants[col]:
                clusters[col].append(index)
     
-    print(clusters)
+    #print(clusters)
     important_TFs = pd.DataFrame(clusters)
     print(important_TFs)
     
--- wp3/Dendro.py
+++ wp3/Dendro.py
@@ -1,25 +1,102 @@
 import pandas as pd
+import numpy as np
 
 def main():
     df = pd.read_table("bindetect_distances.txt", sep="\t")
     
     print(df)
     
-    families = []
+    families = {}
     
-    for col in df:
+    filteredCols = []
+    filteredScores = []
+    
+    for i, col in enumerate(df):
+        colMeanScore = 0
         tmpList = []
+        families[i] =  []
+        
         for index in range(len(df.index)):
         
            
             if df.at[index,col] < 0.5:
                 tmpList.append(df.iloc[:,index].name)
+                colMeanScore = colMeanScore + df.at[index,col]
+        
             
-        if tmpList in families:
+        colMeanScore = colMeanScore / len(tmpList)
+        
+        if tmpList in filteredCols:
             pass
         else:
-            families.append(tmpList)
+            filteredCols.append(tmpList)
+            filteredScores.append(colMeanScore)
     
+    
+    #print(filteredScores)
+    #print(len(filteredScores))
+    #print(df.iloc[:,-2])
+    
+    
+    for index, col in enumerate(df):
+        
+        #tmpScores = {}
+        minMean = 0
+        minName = ""
+        minPos = 0
+        colMeans = []
+        indices = []
+        scores = []
+        
+        for i, element in enumerate(filteredCols):
+            
+            
+            if col in element:
+                
+                scores.append(filteredScores[i])
+                indices.append(i)
+                        #tmpScores[i] = filteredScores[i]
+                        #if filteredScores[i] < minMean:
+                            
+                           # minimum += filteredScores[i]
+                           # minName = col
+                           #minPos = index
+                           
+        tmpMin = 1
+        tmpPos = 0
+        #print(colMeans)
+        for i, score in enumerate(scores):
+            if score > 0 and score < tmpMin:
+                tmpMin = score
+                tmpPos = indices[i]
+        
+                
+            
+                       
+       # if index <= 10:
+            
+            #print(tmpScores.keys())
+            #print(tmpScores.values())
+            #minScore = 1
+            #whichmin = ""
+            #for index, element in enumerate(tmpScores):
+             #   if tmpScores[element] < minScore:
+             #      minScore = tmpScores[element]
+             #       whichmin = element
+            #print(minScore)
+            #print(whichmin)    
+        
+        families[tmpPos].append(col)
+                
+                
+    # for element in families:
+    #     for thing in families[element]:
+    #         if thing.startswith("TBX"):
+                
+    #             print(element)
+    #print(families[94])
+    #print(df.loc[:,"ATF3_MA0605.2"])
+        
     #print(tfs)
     
     # families = []
@@ -31,11 +108,19 @@ def main():
     #                  pass
     #              else:
     #                  families.append(tfs[key])
-    print(families[0],"\n",families[1],"\n",families[2])
         
-        
-        
+    outfile = open("TF_familiesv2.txt", "w")
+    outfile.write("##FamilyName \t TFs \n")
     
+    for index, element in enumerate(families):
+        outfile.write(str(index) + "\t")
+        
+        for TF in families[element]:
+            outfile.write(TF+",")
+        
+        outfile.write("\n")
+        
+    outfile.close()
     
 if __name__ == '__main__':
     main()
