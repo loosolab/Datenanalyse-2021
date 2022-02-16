0d71a0460f2cae294621ee4b02c9f3ab1a320958
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-02-16 23:03:27 +0100

    put together Motof similarity scripts

---
+++ wp5/scripts/getSimilarMotifs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# check if conda evironment is active
+if  [ ! "$CONDA_DEFAULT_ENV" == "TOBIAS_ENV" ]; then
+    echo "Activateing TOBIAS_ENV..."
+    source /opt/miniconda/bin/activate TOBIAS_ENV
+fi
+
+MOTIF_FILES="${@:2}"
+NAME=$1
+# put all motifs in same File
+dt=$(date '+%d-%m-%Y_%H-%M')
+joined="${NAME}_motifs_all_${dt}.meme"
+TOBIAS FormatMotifs --input $MOTIF_FILES --task join --output $joined --format meme
+
+# apply TOBIAS Clustering
+TOBIAS ClusterMotifs --motifs $joined  --clust_method "complete" --dist_method "pcc" --threshold  0.3 --type pdf -o "${NAME}_Cluster"
+
+# changing environments
+echo "Switching environment to MEME_ENV"
+source /opt/miniconda/bin/activate MEME_ENV
+
+# apply TOMTOM clustering of meme suite
+tomtom -o "${NAME}_TOMTOM" -thresh 0.3 -dist pearson -png $joined $joined
+
+conda deactivate
