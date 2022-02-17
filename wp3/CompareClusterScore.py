d8881417cbb65c42ff5dd11be4f855a51ddc296a
Moritz Hobein <moritz.hobein@bio.uni-giessen.de>
2022-02-17 23:55:10 +0100

    adapted scripts to wirk with TF families

--- wp3/CompareClusterScore.py
+++ wp3/CompareClusterScore.py
@@ -18,9 +18,10 @@ def cliParser():
     parser.add_argument('-o', '--outputName', dest='Custom_filename', default="ClusterComparison", help='Sets a custom name for saving the output files')
     parser.add_argument('-z', '--ZScore', dest='Z', default=True, action='store_false', help='if used, skips Z Scores calculation for each TF')
     parser.add_argument('-s', '--shortNames', dest='shortNames', default=False, action='store_true', help='If used, saves the short Name of each TF instead of the output_prefix')
+    parser.add_argument('-f', '--families', dest='families', nargs=1, default="thisisnotafile", help='Family cluster file to group TF families together')
     args = parser.parse_args()
     
-    return args.File, args.Norm, args.Custom_filename, args.Z, args.shortNames
+    return args.File, args.Norm, args.Custom_filename, args.Z, args.shortNames, args.families
 
 #normalizing the scores to the same sum
 def normalizeToSum(scores, fileNames):
@@ -73,16 +74,71 @@ def toZScore(dfNorm, fileNames):
     
     dfZ = dfZ_transposed.transpose()
     
-    print(dfZ)
+    #print(dfZ)
     return dfZ
     
-
-def main():
-    possibleNormMethods = ["Sum","None"]
+def groupFamilies(familyFile, df):
     
-    inputFile, normMethod, outputName, Z, shortNames = cliParser()
-    #outfile = open("ClusterComparison.tsv","a")
+    with open(familyFile, "r") as file:
+        
+        colnames = []
+        
+        for col in df:
+            colnames.append(col)
+        
+        dfT = df.transpose()
+        
+        families = {}
+        
+        for line in file:
+            #print(line)
+            familyName, familyMembers = line.split("\t")
+            familyMembers= familyMembers.split("\n")
+            familyMembers = familyMembers[0].split(",")
+            families[familyName] = familyMembers
+        
+        
+        #print(families)
+        familyScores = {}
+        
+        for familyName in families.keys():
+            #print(familyName)
+            tmpScores = 0
+            memberCounter = 0
+            
+            
+            for col in dfT:
+                #print(col)
+                #print(families[familyName])
+                if col in families[familyName]:
+                    if tmpScores == 0:
+                        tmpScores = dfT[col].tolist()
+                        memberCounter += 1
+                    else:
+                        for i, score in enumerate(dfT[col].tolist()):
+                            tmpScores[i] += score
+                        memberCounter += 1
+                
+                    if memberCounter == len(families[familyName]):
+                        break
+                #print(memberCounter)
+           # print(tmpScores)
+            
+            meanScores = []
+            
+            for score in tmpScores:
+                meanScores.append(score/memberCounter)
+            
+            familyScores[familyName] = meanScores
     
+        
+        df_families = pd.DataFrame(familyScores)
+        df_families = df_families.T
+        df_families.columns = colnames
+       
+        return df_families
+            
+def extractScores(infile, shortNames, normMethod, Z, outputName, familyFile):
     #preparing a list of lists with columns for the TFs and each cluster with the scores for them
     scores = {"TF": []}
     transcriptionFactors = {}
@@ -96,7 +152,7 @@ def main():
     
     
     #inputData = pd.read_table(inputFile[0], sep="\t")
-    file = open(inputFile[0], "r")
+    file = open(infile, "r")
     
     for index, line in enumerate(file):
         if index == 0:
@@ -128,7 +184,7 @@ def main():
         else:
             noN = line.split("\n")
             columns = noN[0].split("\t")
-            print(columns)   
+            #print(columns)   
                 # for element in columns:
                 #     try:
                 #         columns.remove("")
@@ -136,14 +192,14 @@ def main():
                 #         pass
             
             for i, value in enumerate(clusterCol):
-                print(clusterCol)
+                #print(clusterCol)
                 if columns[nameCol] in transcriptionFactors.keys():
                     transcriptionFactors[columns[nameCol]][i] = columns[value]
                 else:
                     transcriptionFactors[columns[nameCol]] = []
                     for cluster in clusterNames:
                         transcriptionFactors[columns[nameCol]].append(0)
-                    print(transcriptionFactors)
+                    #print(transcriptionFactors)
                     transcriptionFactors[columns[nameCol]][i] = columns[value]
                
     file.close()
@@ -163,6 +219,8 @@ def main():
     #normalizes the scores 
     #adjustedScores
     
+    possibleNormMethods = ["Sum","None"]
+    
     if normMethod not in possibleNormMethods:
         print("Normalization method not available, defaulting to 'Sum'")
         dfNormScores = normalizeToSum(scores, clusterNames)
@@ -177,10 +235,42 @@ def main():
         dfZScores = toZScore(dfNormScores, clusterNames)  
     else:
         dfZScores = dfNormScores
-            
     
-    dfZScores.to_csv(outputName + '.tsv', sep="\t")
+    if familyFile != "thisisnotafile":
+        dfFinal = dfZScores
+    else:
+        dfFinal = groupFamilies(familyFile, dfZScores)        
+    
+    colnames = []
+    
+    for col in dfFinal:
+        colnames.append(col)    
+    
+    dfFinal.to_csv(outputName + '.tsv', sep="\t")    
+    
+    #CLARION-file generation
+    with open(outputName+'.tsv',"r") as csv:
+        with open(outputName+'.clarion',"w") as clarion:
+            
+            clarion.write("!format=Clarion\n#key\tlevel\n#TF\tfeature\n")
+            
+            for cluster in colnames:
+                clarion.write("#"+cluster+"\tsample\n")
         
+            for line in csv:
+                if line.startswith("\t"):
+                    clarion.write("TF"+line)
+                else:
+                    clarion.write(line)
+
+def main():
+    
+    inputFile, normMethod, outputName, Z, shortNames, familyFile = cliParser()
+    #outfile = open("ClusterComparison.tsv","a")
+    
+
+    extractScores(inputFile[0], shortNames, normMethod, Z, outputName, familyFile[0])
+
 
  
 if __name__ == '__main__':
