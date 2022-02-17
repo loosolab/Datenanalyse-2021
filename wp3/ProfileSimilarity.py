0a4f2624ae648fc6ef037c740e5ca750a947f22b
Moritz Hobein <moritz.hobein@bio.uni-giessen.de>
2022-02-17 01:18:07 +0100

    Script to cluster TF families now finally works!

--- wp3/ProfileSimilarity.py
+++ wp3/ProfileSimilarity.py
@@ -6,13 +6,16 @@ Created on Fri Feb 11 12:02:36 2022
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-inputFiles = ["liverDTF.txt","lungDTF.txt"]
+
+inputFiles = ["testdaten/liverDTF.txt","testdaten/lungDTF.txt"]
 
 TFs = {}
 
 for file in inputFiles:
     df = pd.read_csv(file, sep="\t")
+    #print(df)
     
     for col in df:
         factors = df[col].tolist()
@@ -28,10 +31,20 @@ for file in inputFiles:
         
         for tf in factors:
             TFs[tf].append(col)
+            
+
+for tf in TFs:
+    for cluster in TFs[tf]:
+
+        plt.scatter(tf, cluster)
+
+plt.legend()
+plt.show()
+
     
 with open("similarity.tsv","w") as outfile:
     
-    outfile.write("#TF\tcelltypes\n")
+    outfile.write("TF\tcelltypes\n")
     
     for tf in TFs:
         outfile.write(tf + "\t")
