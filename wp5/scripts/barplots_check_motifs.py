b3514164cc525184654879415727b8fb237243b1
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-02-25 15:10:03 +0100

    added_seccond plot to barplot script

--- wp5/scripts/barplots_check_motifs.py
+++ wp5/scripts/barplots_check_motifs.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 
 # Initialize data lists for tissue, cell_type und motifs
-data = pd.read_csv('/mnt/workspace_stud/allstud/wp5/runs/check_new_motifs_all.txt', sep='\t')
+data = pd.read_csv('/mnt/workspace_stud/allstud/wp5/runs/check_new_motifs_all.tx', sep='\t')
 tissue_names = pd.unique(data['tissue'])
 
 # create plot for all tissues
@@ -30,6 +30,8 @@ plt.close()
 for tissue in tissue_names:
     # plot the data as bar plot
     tis_data = data[data['tissue'] == tissue]
+
+    ## plot barplot per motif
     fig = sns.countplot(data=tis_data, x="sequence", order=tis_data["sequence"].value_counts().index)
     plt.xticks(rotation=90)
     plt.title("Motif Counts for all new Motifs (" + tissue + ")")
@@ -40,6 +42,21 @@ for tissue in tissue_names:
 
     # save plot
     figure_name = "new_motif_counts_for_" + tissue + "_barplot.png"
+    path = "/mnt/workspace_stud/allstud/wp5/runs/" + tissue + "/"  # path to save the figure
+    fig.savefig(path + figure_name, dpi=fig.dpi)
+    plt.close()
+
+    ## plot bar plot per cluster
+    fig = sns.countplot(data=tis_data, x="cell_type", order=tis_data["cell_type"].value_counts().index)
+    plt.xticks(rotation=90)
+    plt.title("Cell type Counts for all new Motifs (" + tissue + ")")
+    plt.xlabel("Cell types", labelpad=15)
+
+    # show bar plot
+    plt.show()
+
+    # save plot
+    figure_name = figure_name = "motif_counts_" + new_tissue + "_barplot.png"
     path = "/mnt/workspace_stud/allstud/wp5/runs/" + tissue + "/"  # path to save the figure
     fig.savefig(path + figure_name, dpi=fig.dpi)
     plt.close()
