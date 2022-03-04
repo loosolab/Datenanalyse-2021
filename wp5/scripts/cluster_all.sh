9509f7773e242b1b05e04e6f95ada13dc6cbcfa9
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-03-04 15:11:24 +0100

    check for file availability

--- wp5/scripts/cluster_all.sh
+++ wp5/scripts/cluster_all.sh
@@ -4,11 +4,14 @@ PROJECT_DIR=$1
 TEMP_DIR="${PROJECT_DIR}/runs/${2}_motifs"
 CLUSTERING_NAME="liver"
 
+mkdir $TEMP_DIR
 for TISSUE in $PROJECT_DIR/runs/*/; do
 	T_NAME=$(basename $TISSUE)   
     for CT in $TISSUE*/; do
-	CT_NAME=$(basename $CT)
-        cp $CT/motif_discovery_pipeline/3_evaluation/motifs.meme "$TEMP_DIR/${T_NAME}_${CT_NAME}.meme"
+	    CT_NAME=$(basename $CT)
+        if test -f "$CT/motif_discovery_pipeline/3_evaluation/motifs.meme"; then
+            cp $CT/motif_discovery_pipeline/3_evaluation/motifs.meme "$TEMP_DIR/${T_NAME}_${CT_NAME}.meme"
+        fi
     done
 done
 ./getSimilarMotifs.sh $CLUSTERING_NAME $TEMP_DIR/*
