6d80b470cd25a871e1d001bcb63cda26dba6ed53
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-03-18 13:45:04 +0100

    added some documentation

--- wp5/scripts/generate_configs.sh
+++ wp5/scripts/generate_configs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+## Script to generate config for each celltype of each tissue
+
 # TOBIAS output directory
-# Path to tobias directory
+# Path to tobias directory ; TODO
 TBSDIR="/mnt/workspace_stud/allstud/wp3/tissues"
 
+# generate config for each celltype of each tissue
 for TISSUE in $TBSDIR/*/; do
     TIS_NAME=$(basename $TISSUE)
     for SUB_TYPE in ${TISSUE}snakemakeout/footprinting/*_footprints.bw; do
--- wp5/scripts/renameAllMotifs.sh
+++ wp5/scripts/renameAllMotifs.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
+## Script to rename all motifs of the runs with the naming schema Tissue_CellType_Nr
+## Caution: Only the meme files containing all the motifs will be changed. The JASPAR file containing single Motifs will not be changed.
+
 # Path to project directory
 DIR=$1
 
+# Iterate over all tissues and their celltypes and change the motif names.
+echo "Renaming motifs"
 for TISSUE in $DIR/*/; do
     TIS_NAME=$(basename $TISSUE)
     for CT in $TISSUE*/; do
 	    CT_NAME=$(basename $CT)
         PATTERN="${TIS_NAME}_${CT_NAME}"
-        echo $CT
         sed -E -i "s/motif_([[:digit:]]+)([[:blank:]]+)motif_([[:digit:]]+)/${PATTERN}_\1\2${PATTERN}_\3/" $CT/motif_discovery_pipeline/3_evaluation/motifs.meme
     done
 done
+
+echo "Renaming done!"
