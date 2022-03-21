4cb66ba1bfbc67708a7611c63aaf94b1d179040d
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-03-21 15:07:17 +0100

    fixed warnings in renaming script + documentation

--- wp5/scripts/renameAllMotifs.sh
+++ wp5/scripts/renameAllMotifs.sh
@@ -3,7 +3,7 @@
 ## Script to rename all motifs of the runs with the naming schema Tissue_CellType_Nr
 ## Caution: Only the meme files containing all the motifs will be changed. The JASPAR file containing single Motifs will not be changed.
 
-# Path to project directory
+# Path to "runs" directory
 DIR=$1
 
 # Iterate over all tissues and their celltypes and change the motif names.
@@ -12,8 +12,13 @@ for TISSUE in $DIR/*/; do
     TIS_NAME=$(basename $TISSUE)
     for CT in $TISSUE*/; do
 	    CT_NAME=$(basename $CT)
-        PATTERN="${TIS_NAME}_${CT_NAME}"
-        sed -E -i "s/motif_([[:digit:]]+)([[:blank:]]+)motif_([[:digit:]]+)/${PATTERN}_\1\2${PATTERN}_\3/" $CT/motif_discovery_pipeline/3_evaluation/motifs.meme
+        # check if file exists
+        FILE=${CT}/motif_discovery_pipeline/3_evaluation/motifs.meme
+        if test -f "$FILE"; then
+            PATTERN="${TIS_NAME}_${CT_NAME}"
+            sed -E -i "s/motif_([[:digit:]]+)([[:blank:]]+)motif_([[:digit:]]+)/${PATTERN}_\1\2${PATTERN}_\3/" $FILE
+        fi
+        
     done
 done
 
