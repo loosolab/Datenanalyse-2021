ca529960f899e33c98a05c2ff8436ee1464e3f9d
Timo Bäuerlein <timo.baeuerlein11@googlemail.com>
2022-03-23 13:52:44 +0100

    Some changes in README.

--- wp5/scripts/analyze_GO_and_pathway.py
+++ wp5/scripts/analyze_GO_and_pathway.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import argparse
 
-parser = argparse.ArgumentParser(description='Evaluate differntial binding')
+parser = argparse.ArgumentParser(description='Analyze Gene Ontology (GO) and pathway of the new found motifs')
 parser.add_argument('--in-dir', metavar="PATH/TO/DIR", help='Path to directory where the motif discovery runs are stored', required=True )
 parser.add_argument('--motifs', metavar='MOTIF_CLUSER.yml', help='Output of the motif clustering with TOBIAS', required=True)
 parser.add_argument('--out', metavar="FILENAME_prefix", help="Prefix of how the output files should be named.", required=True)
