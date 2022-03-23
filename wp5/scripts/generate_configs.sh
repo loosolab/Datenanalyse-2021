89fa90eefd72528dc097cbde54dc775d8a306e81
gnnpl <gianna.isabel.pohl@mni.thm.de>
2022-03-23 14:40:35 +0100

    bug fix in generate configs

--- wp5/scripts/generate_configs.sh
+++ wp5/scripts/generate_configs.sh
@@ -12,6 +12,6 @@ for TISSUE in $TBSDIR/*/; do
     TIS_NAME=$(basename $TISSUE)
     for SUB_TYPE in ${TISSUE}snakemakeout/footprinting/*_footprints.bw; do
 	    SUB_NAME=$(basename "$SUB_TYPE" "_footprints.bw")
-	    $(SPATH)/utils/generate_configs_yml.sh $TIS_NAME $SUB_NAME
+	    ${SPATH}/utils/generate_configs_yml.sh $TIS_NAME $SUB_NAME
     done    
 done
