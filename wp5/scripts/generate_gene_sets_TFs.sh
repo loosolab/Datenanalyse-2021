854e4e7f8b0c399a21cc51f71afc4a8282fd59d4
Timo Bäuerlein <timo.baeuerlein11@googlemail.com>
2022-03-23 13:09:56 +0100

    Generated the two analyzation files for the annotation analysis.

--- wp5/scripts/generate_gene_sets_TFs.sh
+++ wp5/scripts/generate_gene_sets_TFs.sh
@@ -67,82 +67,82 @@ for TISSUE in $DIR/*; do
         # compare the counts
         if [ $TF_COUNTER -gt $PUNCTUATION_CHECKER ]; then
             for TF in $FILE_PATH_ANNOTATION/*; do
-                echo "$TF"
-                TF=$(echo $TF | rev | cut -d'/' -f-1 | rev)    # extract the TF name
-                echo "$TF"
-                # check if the TF has already been analyzed
-                TF_CHECKER=$(cat $FILE_1K | grep -c $TF)   #  check if the TF is in the file
-                if [ $TF_CHECKER -eq 0 ]; then
-                    echo "$TF"
-                    DT_TF=$(date '+%d/%m/%Y %H:%M:%S')   # start time of the TF
-                    echo "Starting ${TF} at ${DT_TF}."
-                                
-                    GENE_SET_NAME_1K="#${TF}_1k" # column 1 -> 1k
-                    GENE_SET_NAME_2K="#${TF}_2k" # column 1 -> 2k
-                    GENES_1K=()                  # columns after 2 -> 1k
-                    GENES_2K=()                  # columns after 2 -> 2k
-                                   
-                    cd $FILE_PATH_ANNOTATION/$TF
+                # check if the file is a directory -> TF and nothing else
+                if [ -d $TF ]; then
+                    TF=$(echo $TF | rev | cut -d'/' -f-1 | rev)    # extract the TF name
+                    # check if the TF has already been analyzed
+                    TF_CHECKER=$(cat $FILE_1K | grep -c $TF)   #  check if the TF is in the file
+                    if [ $TF_CHECKER -eq 0 ]; then
+                        DT_TF=$(date '+%d/%m/%Y %H:%M:%S')   # start time of the TF
+                        echo "Starting ${TF} at ${DT_TF}."
                                     
-                    # read *overview.txt
-                    # check if the feature = gene $11, check the distance $16, add the gene name $20 to array
-                    OVERVIEW=($(find . | grep overview.txt | sed 's,./,,g'))
-                    GENES_1K=($(cat $OVERVIEW | awk -F'\t' '{if ($11 == "gene" && $16 != "NA" && $16 <= 1000 && $20 != "NA") print $20;}'))
-                    GENES_2K=($(cat $OVERVIEW | awk -F'\t' '{if ($11 == "gene" && $16 != "NA" && $16 <= 2000 && $20 != "NA") print $20}'))
-                                
-                    # only sort genes -> no unique genes
-                    SORTED_GENES_1K=($(echo "${GENES_1K[@]}" | tr ' ' '\n' | sort | tr '\n' ' '))
-                    SORTED_GENES_2K=($(echo "${GENES_1K[@]}" | tr ' ' '\n' | sort | tr '\n' ' '))
-                                
-                    # only choose the unique genes
-                    SORTED_UNIQUE_GENES_1K=($(echo "${GENES_1K[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-                    SORTED_UNIQUE_GENES_2K=($(echo "${GENES_2K[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-                                                                
-                    # counter for indices in arrays (SORTED_UNIQUE_GENES_1K)
-                    COUNTER_ARR_1K=0
-                    # fill the *1k.txt file with data
-                    echo -e "${GENE_SET_NAME_1K}" >> $FILE_1K   # insert the gene set name to the file (1k)
-                    # append the genes to the assciated gene set name (1k)
-                    for elem in "${SORTED_UNIQUE_GENES_1K[@]}"; do
-                        echo -e "${SORTED_UNIQUE_GENES_1K[COUNTER_ARR_1K]}" >> $FILE_1K
-                        let COUNTER_ARR_1K++
-                    done
-                    echo -e "" >> $FILE_1K    # insert a new line at the end of the motif data
+                        GENE_SET_NAME_1K="#${TF}_1k" # column 1 -> 1k
+                        GENE_SET_NAME_2K="#${TF}_2k" # column 1 -> 2k
+                        GENES_1K=()                  # columns after 2 -> 1k
+                        GENES_2K=()                  # columns after 2 -> 2k
+                                    
+                        cd $FILE_PATH_ANNOTATION/$TF
+                                        
+                        # read *overview.txt
+                        # check if the feature = gene $11, check the distance $16, add the gene name $20 to array
+                        OVERVIEW=($(find . | grep overview.txt | sed 's,./,,g'))
+                        GENES_1K=($(cat $OVERVIEW | awk -F'\t' '{if ($11 == "gene" && $16 != "NA" && $16 <= 1000 && $20 != "NA") print $20;}'))
+                        GENES_2K=($(cat $OVERVIEW | awk -F'\t' '{if ($11 == "gene" && $16 != "NA" && $16 <= 2000 && $20 != "NA") print $20}'))
+                                    
+                        # only sort genes -> no unique genes
+                        SORTED_GENES_1K=($(echo "${GENES_1K[@]}" | tr ' ' '\n' | sort | tr '\n' ' '))
+                        SORTED_GENES_2K=($(echo "${GENES_1K[@]}" | tr ' ' '\n' | sort | tr '\n' ' '))
+                                    
+                        # only choose the unique genes
+                        SORTED_UNIQUE_GENES_1K=($(echo "${GENES_1K[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+                        SORTED_UNIQUE_GENES_2K=($(echo "${GENES_2K[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+                                                                    
+                        # counter for indices in arrays (SORTED_UNIQUE_GENES_1K)
+                        COUNTER_ARR_1K=0
+                        # fill the *1k.txt file with data
+                        echo -e "${GENE_SET_NAME_1K}" >> $FILE_1K   # insert the gene set name to the file (1k)
+                        # append the genes to the assciated gene set name (1k)
+                        for elem in "${SORTED_UNIQUE_GENES_1K[@]}"; do
+                            echo -e "${SORTED_UNIQUE_GENES_1K[COUNTER_ARR_1K]}" >> $FILE_1K
+                            let COUNTER_ARR_1K++
+                        done
+                        echo -e "" >> $FILE_1K    # insert a new line at the end of the motif data
 
-                    # counter for indices in arrays (SORTED_UNIQUE_GENES_2K)
-                    COUNTER_ARR_2K=0
-                    # fill the *2k.txt file with data
-                    echo -e "${GENE_SET_NAME_2K}" >> $FILE_2K   # insert the gene set name to the file (2k)
-                    # append the genes to the assciated gene set name (2k)
-                    for elem in "${SORTED_UNIQUE_GENES_2K[@]}"; do
-                        echo -e "${SORTED_UNIQUE_GENES_2K[COUNTER_ARR_2K]}" >> $FILE_2K
-                        let COUNTER_ARR_2K++
-                    done
-                    echo -e "" >> $FILE_2K    # insert a new line at the end of the motif data
-                                
-                    # counter for indices in arrays (SORTED_GENES_1K)
-                    COUNTER_ARR_1K_ALL=0
-                    # fill the *1k_all.txt file with data
-                    echo -e "${GENE_SET_NAME_1K}" >> $FILE_1K_ALL   # insert the gene set name to the file (1k_all)
-                    # append the genes to the assciated gene set name (1k_all)
-                    for elem in "${SORTED_GENES_1K[@]}"; do
-                        echo -e "${SORTED_GENES_1K[COUNTER_ARR_1K_ALL]}" >> $FILE_1K_ALL
-                        let COUNTER_ARR_1K_ALL++
-                    done
-                    echo -e "" >> $FILE_1K_ALL    # insert a new line at the end of the motif data
+                        # counter for indices in arrays (SORTED_UNIQUE_GENES_2K)
+                        COUNTER_ARR_2K=0
+                        # fill the *2k.txt file with data
+                        echo -e "${GENE_SET_NAME_2K}" >> $FILE_2K   # insert the gene set name to the file (2k)
+                        # append the genes to the assciated gene set name (2k)
+                        for elem in "${SORTED_UNIQUE_GENES_2K[@]}"; do
+                            echo -e "${SORTED_UNIQUE_GENES_2K[COUNTER_ARR_2K]}" >> $FILE_2K
+                            let COUNTER_ARR_2K++
+                        done
+                        echo -e "" >> $FILE_2K    # insert a new line at the end of the motif data
+                                    
+                        # counter for indices in arrays (SORTED_GENES_1K)
+                        COUNTER_ARR_1K_ALL=0
+                        # fill the *1k_all.txt file with data
+                        echo -e "${GENE_SET_NAME_1K}" >> $FILE_1K_ALL   # insert the gene set name to the file (1k_all)
+                        # append the genes to the assciated gene set name (1k_all)
+                        for elem in "${SORTED_GENES_1K[@]}"; do
+                            echo -e "${SORTED_GENES_1K[COUNTER_ARR_1K_ALL]}" >> $FILE_1K_ALL
+                            let COUNTER_ARR_1K_ALL++
+                        done
+                        echo -e "" >> $FILE_1K_ALL    # insert a new line at the end of the motif data
 
-                    # counter for indices in arrays (SORTED_GENES_2K)
-                    COUNTER_ARR_2K_ALL=0
-                    # fill the *2k_all.txt file with data
-                    echo -e "${GENE_SET_NAME_2K}" >> $FILE_2K_ALL   # insert the gene set name to the file (2k_all)
-                    # append the genes to the assciated gene set name (2k_all)
-                    for elem in "${SORTED_GENES_2K[@]}"; do
-                        echo -e "${SORTED_GENES_1K[COUNTER_ARR_2K_ALL]}" >> $FILE_2K_ALL
-                        let COUNTER_ARR_2K_ALL++
-                    done
-                    echo -e "" >> $FILE_2K_ALL    # insert a new line at the end of the motif data
-                else
-                    echo "The transcription factor $TF of the tissue $TISSUE was already analyzed."
+                        # counter for indices in arrays (SORTED_GENES_2K)
+                        COUNTER_ARR_2K_ALL=0
+                        # fill the *2k_all.txt file with data
+                        echo -e "${GENE_SET_NAME_2K}" >> $FILE_2K_ALL   # insert the gene set name to the file (2k_all)
+                        # append the genes to the assciated gene set name (2k_all)
+                        for elem in "${SORTED_GENES_2K[@]}"; do
+                            echo -e "${SORTED_GENES_1K[COUNTER_ARR_2K_ALL]}" >> $FILE_2K_ALL
+                            let COUNTER_ARR_2K_ALL++
+                        done
+                        echo -e "" >> $FILE_2K_ALL    # insert a new line at the end of the motif data
+                    else
+                        echo "The transcription factor $TF of the tissue $TISSUE was already analyzed."
+                    fi
                 fi
             done
         else
