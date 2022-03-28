f0cff13b193242aff9b67f6f73e032a2c09c5f8a
nico-haeusser <65766169+nico-haeusser@users.noreply.github.com>
2022-03-28 23:15:41 +0200

    update comments in parsebam.py

--- wp2/docker/parsebam.py
+++ wp2/docker/parsebam.py
@@ -5,58 +5,26 @@ print("start")
 import pysam
 import re
 import sys
+# Bamfile to parse
 BAMFILE=sys.argv[1]
+# read Bamfile
 samfile = pysam.AlignmentFile(BAMFILE+".bam","rb",ignore_truncation=True)
+# write Bamfile
 output = pysam.AlignmentFile(BAMFILE+"_parsed.bam","wb",template=samfile)
-cnt = 0
-print("test")
+
+# iterate over lines in Bamfile
 for read in samfile:
-	if cnt == 0:
-		#print(read)
-		#print("----------------")
-		#print(read.query_name)
-		#print("CB:"+read.query_name)
+		# save query without barcode
 		query = str(re.findall(":[:A-Z0-9]+",read.query_name)[0])[1:]
-		#print(query)
+		# read barcode from query
 		barcode = str(re.findall("^[A-Z]+",read.query_name)[0])
-		#print(barcode)
-		#print("---------------")
-		#cnt += 1
 		read_tmp = read
-		#read_tmp.query_name = 
-		#print("query_name:"+read.query_name)
-		#read_tmp.query_sequence = 
-		#print("query_sequence:"+read.query_sequence)
-		#read_tmp.flag = 
-		#print("flag:"+str(read.flag))
-		#read_tmp.reference_id = 
-		#print("reference_id:"+str(read.reference_id))
-		#read_tmp.mapping_quality = 
-		#print("mapping_quality:"+str(read.mapping_quality))
-		#read_tmp.cigar = 
-		#print("cigar:"+str(read.cigar))
-		#read_tmp.next_reference_id = 
-		#print("next_reference_id:"+str(read.next_reference_id))
-		#read_tmp.next_reference_start = 
-		#print("next_reference_start:"+str(read.next_reference_start))
-		#read_tmp.template_lenght = 
-
-
-		#print("tags:"+str(read_tmp.tags[1]))
+		# add barcode to tags
 		read_tmp.tags += [("CB",barcode)]
-		#print("tags:"+str(read_tmp.tags))
+		# override query
 		read_tmp.query_name=query
-		
-
-
-
-
-
-
-		#print("xxx")
+		# write line
 		output.write(read_tmp)
-		#print("xxx2")
 
-#print("test4")
 samfile.close()
 output.close()
