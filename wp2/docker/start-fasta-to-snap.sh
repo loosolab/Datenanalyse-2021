199b4a9ba068980cbda1b902be4271b7503fe706
nico-haeusser <65766169+nico-haeusser@users.noreply.github.com>
2022-03-28 23:38:12 +0200

    Update start-fasta-to-snap.sh

--- wp2/docker/start-fasta-to-snap.sh
+++ wp2/docker/start-fasta-to-snap.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-##TODO for loop over all fastq
-
-#name_tissue="ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver_Rep1.demultiplexed"
-#fasta_path="/mnt/data/fastq"
-
-#name_tissue="ENC-1LGRB-194-SM-A9HOW_snATAC_colon_transverse_Rep1.demultiplexed"
 name_tissue=$1
 echo "name_tissue="$name_tissue
-fasta_path="/mnt/data/fastq_data/renlab.sdsc.edu/kai/data/fastq/first/"
+#fasta_path="/mnt/data/fastq_data/renlab.sdsc.edu/kai/data/fastq/first/"
+# path to files
+fasta_path=$4
 
-#mkdir "/mnt/testing/$name_tissue"
+# fasta files
 fasta1="$fasta_path/$name_tissue""_Rep1.demultiplexed"".R1.fastq.gz"
 fasta2="$fasta_path/$name_tissue""_Rep1.demultiplexed"".R2.fastq.gz"
 
@@ -30,8 +26,4 @@ exec "$3"fasta-to-snap.sh \
 --short-name=$2 \
 --path=$3
 
-#--fasta1=/mnt/data/fastq/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver_Rep1.demultiplexed.R1.fastq.gz \
-#--fasta2=/mnt/data/fastq/ENC-1LGRB-069-SM-A8WNZ_snATAC_right_lobe_of_liver_Rep1.demultiplexed.R2.fastq.gz \
 
-
-#mkdir $name_tissue
