e144ec9ac8b30c181e8b3beffd76e05705a293cf
nico-haeusser <65766169+nico-haeusser@users.noreply.github.com>
2022-03-28 23:17:40 +0200

    update comments

--- wp2/docker/narrowpeaks_to_bed.py
+++ wp2/docker/narrowpeaks_to_bed.py
@@ -4,13 +4,19 @@ import os
 import re
 import sys
 
+# all files in folder
 files = os.listdir(sys.argv[1])
+# print files
 print(files)
+# iterate over files
 for file in files:
+	# only .narrowpeak files
         if re.match(".*narrowPeak$",file):
+	    # create .bed file
             new_file_name = ".".join(file.split(".")[:-1])+".bed"
             with open(sys.argv[1]+new_file_name, 'w') as outfile, open(sys.argv[1]+file, 'r') as infile:
-                for line in infile:
+                # read chr, start and stop from narrowpeak file and write them in .bed file
+		for line in infile:
                     start = line.split()[1]
                     stop = line.split()[2]
                     chr = re.findall("chr[0-9]*",line.split()[0])[0]
